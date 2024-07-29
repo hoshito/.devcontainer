f2017e941220188eb7b02605f6c12699f3e4e9a6
hoshito <10190139+hoshito@users.noreply.github.com.>
2024-07-29 04:40:22 +0000

    update

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -18,7 +18,7 @@ opt.autoread = true
 -- マウス無効
 opt.mouse = ""
 
---opt.autochdir = true
+-- ディレクトリ移動
 vim.keymap.set("n", "<leader>cd", function ()
     vim.cmd(":cd %:h")
     vim.cmd(":pwd")
@@ -30,3 +30,55 @@ end)
 vim.keymap.set("n", "<leader>p", function ()
     vim.cmd(":pwd")
 end)
+--opt.autochdir = true
+
+-- 開業時にコメントアウトしないようにする
+vim.api.nvim_create_autocmd('FileType', {
+  group = vim.api.nvim_create_augroup('turn_off_auto_commenting', {}),
+  pattern = '*',
+  command = [[setlocal fo-=cro]]
+})
+
+-- 言語に応じてファイル実行
+local function run_file(file_runners)
+    local filetype = vim.bo.filetype
+    local interpreter = file_runners[filetype]
+    if interpreter then
+        vim.cmd('write')  -- ファイルを保存
+        vim.cmd('TermExec cmd="' .. interpreter .. ' ' .. vim.fn.expand('%:p') .. '"')
+    else
+        print("このファイルタイプの実行方法が定義されていません: " .. filetype)
+    end
+end
+vim.api.nvim_set_keymap('n', '<leader>r', '', {
+    noremap = true,
+    silent = true,
+    callback = function()
+        local file_runners = {
+            python = "python3",
+            javascript = "node",
+            ruby = "ruby",
+        }
+        run_file(file_runners)
+    end
+})
+
+
+-- セッション管理
+local session_file = vim.fn.stdpath("config") .. "/last_session.vim"
+-- Neovim終了時にセッションを自動保存
+vim.api.nvim_create_autocmd("VimLeavePre", {
+  callback = function()
+    vim.cmd("mksession! " .. session_file)
+  end,
+})
+-- セッションを読み込むためのコマンド
+vim.api.nvim_create_user_command('LoadLastSession', function()
+  if vim.fn.filereadable(session_file) == 1 then
+    vim.cmd("source " .. session_file)
+    print("前回のセッションを読み込みました。")
+  else
+    print("保存されたセッションが見つかりません。")
+  end
+end, {})
+
--- Dockerfile
+++ Dockerfile
@@ -1,18 +1,20 @@
-FROM ubuntu:latest
+FROM ubuntu:20.04
 
 RUN bash
 
 RUN apt update -y && \
     apt upgrade -y && \
-    apt install -y curl git neovim ripgrep fd-find python3.12-venv build-essential && \
+    apt install -y curl git neovim ripgrep fd-find fzf && \
     ln -s  $(which fdfind) /usr/local/bin/fd
 
 # for javascript
-RUN apt install -y nodejs npm && \
+RUN DEBIAN_FRONTEND=noninteractive apt install -y nodejs npm && \
     npm install n -g && \
     n stable && \
     apt purge -y nodejs npm && \
     apt autoremove -y
+
+RUN DEBIAN_FRONTEND=noninteractive apt install -y binutils build-essential golang sysstat python3-matplotlib python3-pil fonts-takao fio qemu-kvm virt-manager libvirt-clients virtinst jq docker.io containerd libvirt-daemon-system
 
 COPY .config /root/.config
 COPY .bash_aliases /root/.bash_aliases
@@ -21,4 +23,8 @@ RUN nvim +:UpdateRemotePlugins +qa
 RUN nvim --headless -c "MasonInstallAll" -c qall
 #RUN nvim --headless -c "MasonInstall lua-language-server" -c qall
 #RUN nvim --headless -c "MasonInstall python-lsp-server" -c qall
+
+RUN adduser `id -un` libvirt && \
+    adduser `id -un` libvirt-qemu && \
+    adduser `id -un` kvm
 
